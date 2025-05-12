cd9739b1c06630ca05fcf6220dd1d6459a0342aa
Catapultam Habeo <alex@catapult.am>
2025-05-12 10:08:41 -0500

    update scripting

--- global/#TPTriggerN.pl
+++ global/#TPTriggerN.pl
@@ -7,20 +7,10 @@ sub EVENT_SPAWN {
 }
 
 sub EVENT_ENTER {    
-    quest::debug("Attempting to update attunement point...");
-
-    # Get the race-specific waypoints for the client's base race
-    my $race_specific_waypoints = plugin::GetRaceSpecificWaypoint($client->GetBaseRace());
-
-    # Check if the current zone is one of the race-specific waypoints
-    if (grep { $_ eq $zonesn } @$race_specific_waypoints) {
-        $client->Message(263, "This location is so familiar that you would never be able to properly explain it to another.");
+    if ($client->WaypointUnlock($zonesn)) {
+        $client->Message(263, "This place seems familiar. You are sure to remember it later.");
     } else {
-        if (plugin::AddWaypoint($zonesn, $client)) {
-            $client->Message(263, "This place seems familiar. You are sure to remember it later.");
-        } else {
-            $client->Message(263, "You know this place well.");
-        }
-    }
+        $client->Message(263, "You know this place well.");
+    }    
 }
 
